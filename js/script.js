f0bcad53f88fea911c0c712ac148de3c34d2dba2
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2022-06-09 19:56:36 -0400

    added team section

--- js/script.js
+++ js/script.js
@@ -4,4 +4,20 @@
 $(window).on('load', function() {
     $('#status').fadeOut();
     $('#preloader').delay(350).fadeOut('slow');
-})
+});
+
+/* ================================
+|   |   |   Team
+================================ */
+$(function () {
+    $("#team-members").owlCarousel({
+        items: 2,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>']
+    });
+});
