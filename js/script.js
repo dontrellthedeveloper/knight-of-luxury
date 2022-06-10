67620be977dfbe96df8785d7bfff79b133dca008
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2022-06-09 22:35:05 -0400

    added stats section

--- js/script.js
+++ js/script.js
@@ -109,3 +109,16 @@ $(function() {
         navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>']
     });
 });
+
+
+/* ================================
+|   |   |   Stats
+================================ */
+$(function () {
+
+    $(".counter").counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+});
